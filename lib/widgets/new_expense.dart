aa9e4b88b82535070b055eb32e12255473b1afa1
mardybum8737 <36396926+mardybum8737@users.noreply.github.com>
2024-01-30 01:09:03 +0900

    dropdownmenu

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense_model.dart';
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -16,9 +17,27 @@ class _NewExpenseState extends State<NewExpense> {
   // }
 
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    final pickedDate = await showDatePicker(
+      context: context,
+      firstDate: firstDate,
+      lastDate: now,
+      initialDate: now,
+    );
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
+
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
@@ -32,23 +51,80 @@ class _NewExpenseState extends State<NewExpense> {
             // onChanged: _saveTitleInput,
             controller: _titleController,
             maxLength: 50,
-            decoration: const InputDecoration(label: Text('Title')),
+            decoration: const InputDecoration(
+              label: Text('Title'),
+            ),
             // keyboardType: TextInputType.text, // default
           ),
           Row(
             children: [
-              ElevatedButton(
+              Expanded(
+                child: TextField(
+                  // onChanged: _saveTitleInput,
+                  controller: _amountController,
+                  maxLength: 50,
+                  decoration: const InputDecoration(
+                    label: Text('Amount'),
+                    prefixText: '\$ ',
+                  ),
+                  keyboardType: TextInputType.number, // default
+                ),
+              ),
+              const SizedBox(width: 16),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      _selectedDate == null
+                          ? 'No date selected'
+                          : formatter.format(_selectedDate!),
+                    ),
+                    IconButton(
+                        onPressed: _presentDatePicker,
+                        icon: const Icon(
+                          Icons.calendar_month,
+                        ))
+                  ],
+                ),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              DropdownButton(
+                items: Category.values
+                    .map(
+                      (category) => DropdownMenuItem(
+                        child: Text(
+                          category.name.toString(),
+                        ),
+                      ),
+                    )
+                    .toList(),
+                onChanged: (value) {},
+              ),
+              TextButton(
                 onPressed: () {
-                  print(_titleController.text);
+                  Navigator.pop(context);
                 },
-                child: const Text('Save Expense'),
+                child: const Text('Cancel'),
               ),
               const SizedBox(
                 width: 16,
               ),
               ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.deepPurple,
+                  foregroundColor: Colors.white,
+                ),
                 onPressed: () {},
-                child: const Text('Cancel'),
+                child: const Text('Save Expense'),
               ),
             ],
           )
